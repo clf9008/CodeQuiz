aeadc36dff02f10c383dafd4e16a14bc1670eb45
Follett <clf9008@gmail.com>
2020-10-18 20:59:14 -0600

    created array for quiz questions

--- script.js
+++ script.js
@@ -10,3 +10,87 @@ const choice2 = document.getElementById("2");
 const choice3 = document.getElementById("3");
 const quizProgress = document.getElementById("quizProgress");
 const userScore = document.getElementById("userScore");
+
+let questions = [
+
+    {
+    question : "what does this question ask?"
+    imgSrc : "img.png",
+    choice1 : "This is the first answer",
+    choice2 : "This is the second answer",
+    choice3 : "this is the third answer"
+    correct : "1"
+    },
+    {
+        question : "what does this question ask #2?"
+        imgSrc : "img2.png",
+        choice1 : "This is the first answer",
+        choice2 : "This is the second answer",
+        choice3 : "this is the third answer"
+        correct : "1"
+    }
+    {
+        question : "what does this question ask?"
+        imgSrc : "img.png",
+        choice1 : "This is the first answer",
+        choice2 : "This is the second answer",
+        choice3 : "this is the third answer"
+        correct : "1"
+    }
+    {
+        question : "what does this question ask?"
+        imgSrc : "img.png",
+        choice1 : "This is the first answer",
+        choice2 : "This is the second answer",
+        choice3 : "this is the third answer"
+        correct : "1"
+    }
+    {
+        question : "what does this question ask?"
+        imgSrc : "img.png",
+        choice1 : "This is the first answer",
+        choice2 : "This is the second answer",
+        choice3 : "this is the third answer"
+        correct : "1"
+    }
+    {
+        question : "what does this question ask?"
+        imgSrc : "img.png",
+        choice1 : "This is the first answer",
+        choice2 : "This is the second answer",
+        choice3 : "this is the third answer"
+        correct : "1" 
+    }
+    {
+        question : "what does this question ask?"
+    imgSrc : "img.png",
+    choice1 : "This is the first answer",
+    choice2 : "This is the second answer",
+    choice3 : "this is the third answer"
+    correct : "1"
+    }
+    {
+        question : "what does this question ask?"
+        imgSrc : "img.png",
+        choice1 : "This is the first answer",
+        choice2 : "This is the second answer",
+        choice3 : "this is the third answer"
+        correct : "1"   
+    }
+    {
+        question : "what does this question ask?"
+        imgSrc : "img.png",
+        choice1 : "This is the first answer",
+        choice2 : "This is the second answer",
+        choice3 : "this is the third answer"
+        correct : "1"   
+    }
+    {
+        question : "what does this question ask?"
+        imgSrc : "img.png",
+        choice1 : "This is the first answer",
+        choice2 : "This is the second answer",
+        choice3 : "this is the third answer"
+        correct : "1"   
+    }
+];  
