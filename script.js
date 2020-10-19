7df9ca72b72e0f69b27fa741e71a6c1c7e26353d
Follett <clf9008@gmail.com>
2020-10-18 21:39:37 -0600

    added function render question timer and function to check answers

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const start = document.getElementById("start");
+//constants on the application display
+const start = document.getElementById("start"); 
 const quiz = document.getElementById("quiz");
 const questionImage = document.getElementById("questionImage");
 const question = document.getElementById("question");
@@ -11,15 +12,16 @@ const choice3 = document.getElementById("3");
 const quizProgress = document.getElementById("quizProgress");
 const userScore = document.getElementById("userScore");
 
+//Questions array with 10 questions for quiz
 let questions = [
 
     {
-    question : "what does this question ask?"
-    imgSrc : "img.png",
-    choice1 : "This is the first answer",
-    choice2 : "This is the second answer",
-    choice3 : "this is the third answer"
-    correct : "1"
+        question : "what does this question ask?"
+        imgSrc : "img.png",
+        choice1 : "This is the first answer",
+        choice2 : "This is the second answer",
+        choice3 : "this is the third answer"
+        correct : "1"
     },
     {
         question : "what does this question ask #2?"
@@ -63,11 +65,11 @@ let questions = [
     }
     {
         question : "what does this question ask?"
-    imgSrc : "img.png",
-    choice1 : "This is the first answer",
-    choice2 : "This is the second answer",
-    choice3 : "this is the third answer"
-    correct : "1"
+        imgSrc : "img.png",
+        choice1 : "This is the first answer",
+        choice2 : "This is the second answer",
+        choice3 : "this is the third answer"
+        correct : "1"
     }
     {
         question : "what does this question ask?"
@@ -94,10 +96,12 @@ let questions = [
         correct : "1"   
     }
 ];  
-array = [ a, b, c, d] 
+
+//setting the application to display the first condition in the questions array
 let lastquestionIndex = questions.length - 1; 
 let runningquestionIndex = 0;
 
+//function to render quesiton on application
 function questionRender (){
     let q = questions[runningquestionIndex];
     qImg.innerHTML = "<img src=" + q.imgSrc + ">";
@@ -107,15 +111,58 @@ function questionRender (){
     choice3.innerHTML = q.choice3;
 }
 
+//function to create quiz progress display on application 
 function progressRender(){
     for (let qIndex = 0; qIndex < lastquestionIndex.length; qIndex++) {
         quizProgress.innerHTML +="<div class='prog'id=" + qIndex + "></div>";
         }
 }
+//funciton for display if question is answered correctly
 function correctAnswer(){
     document.getElementById(runningQuestionIndex).style.backgroundColor = "green";
 }
+//funciton for display if question is answered incorrectly
 function incorrectAnswer(){
     document.getElementById(runningquestionIndex).style.backgroundColor = "red";
 }
+//constant of question timer that will be displayed on application
+const questionTime = 10; //10 seconds every question
+const gaugeWidth = 150;
+let count   =     0;
+const gaugeProgressUnit = gaugeWidth/questionTime;
+//function to for question timer to display progress left for each question
+
+function counterRender(){
+    if( count ,= questionTime ){
+        counterRender.innerHTML = count;
+        timerGauge.style.width = gaugeProgressUnit * count + "px" ;
+        count++;
+    }else{
+        count = 0;
+        incorrectAnswer();
+        if( runningquestionIndex < lastquestionIndex){
+            runningquestionIndex++;
+            questionRender();
+
+        }else{ clearInterval(TIMER);
+        scoreRender(); }
+    }
+}
+
+function checkAnswer(answer){
+    if(questions[runningquestionIndex].correct == answer){
+        score++;
+        correctAnswer();
+    }else{
+        incorrectAnswer();
+    }
+    if (runningquestionIndex < lastquestionIndex){
+        count = 0;
+        runningquestionIndex++;
+        questionRender();
+    }else{
+        clearInterval(TIMER);
+        scoreRender();
+    }
+    }
 
