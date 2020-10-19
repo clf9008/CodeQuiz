2b807b821c77bebfeeee4dadd584ddf95f1c70a9
Follett <clf9008@gmail.com>
2020-10-19 16:53:22 -0600

    added highscore.html and highscore.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// select all elements
+// elements to be stored in Global Memeory
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
@@ -11,7 +11,7 @@ const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
 
-// create our questions
+// creating the questions that will be asked
 let questions = [
     {
         question : "In what year did Dale Earnhardt win the Rookie of the Year award",
@@ -44,7 +44,7 @@ let questions = [
     },
     {
         question : "Who was Dale Earnhardt's greatest rival?",
-        imgSrc : "assets/JeffGordon.jpg",
+        imgSrc : "assets/Rivals.jpg",
         choice1 : "Jeff Gordon",
         choice2 : "Richard Petty",
         choice3 : "Tony Stewart",
@@ -52,18 +52,17 @@ let questions = [
     },
 ];
 
-
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-let count = 90;
-const questionTime = 90; // 90s
-const subtractTime = 3;
+//declaring variables to be stored in Global Memory
+const lastQuestion = questions.length - 1; //variable to move questions forward
+let runningQuestion = 0; //variable for the current question pulled up in the 'question' array
+let count = 90; //the time that the user will have on the quiz --90 seconds
+const questionTime = 90; // 90seconds time on quiz
 const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / questionTime;
+const gaugeUnit = gaugeWidth / questionTime; 
 let TIMER;
-let score = 0;
+let score = 0; 
 
-// render a question
+// function to call a question up one webpage is loaded
 function renderQuestion(){
     let q = questions[runningQuestion];
     
@@ -73,91 +72,90 @@ function renderQuestion(){
     choice2.innerHTML = q.choice2;
     choice3.innerHTML = q.choice3;
 }
-
+//Click event listener to start the quiz
 start.addEventListener("click",startQuiz);
 
-// start quiz
+// start quiz function
 function startQuiz(){
     start.style.display = "none";
     renderQuestion();
     quiz.style.display = "block";
     renderProgress();
     renderCounter();
-    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s handling correct timing
 }
 
-// render progress
+//fucntion to show the progress of the user on the quiz
 function renderProgress(){
     for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
         progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
     }
 }
-
-// counter render
-
+//function to show the time left on the quiz
 function renderCounter(){
-    if(count <= questionTime){
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count--
-    }else{
-        count = 90;
-        // change progress color to red
-        answerIsWrong();
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
-            renderQuestion();
-        }else{
-            // end the quiz and show the score
-            clearInterval(TIMER);
-            scoreRender();
-        }
-    }
+    //start count at 90 and decremement 
+// count = 90
+console.log(count)
+counter.innerHTML = count;
+timeGauge.style.width = count * gaugeUnit + "px";
+ if (count<= 0) {
+   count=0; 
+} else {
+    count--;
+    
+   
+}
+    
 }
 
 
-// checkAnwer
+//function to check the answer of a quetions, If it is correct, progress shows green, else if it is incorrect, it shows progress red
 
 function checkAnswer(answer){
     if( answer == questions[runningQuestion].correct){
         // answer is correct
         score++;
-        // change progress color to green
+        // change progress color to green if correct
         answerIsCorrect();
     }else{
-        // answer is wrong
-        // change progress color to red
+        // change progress color to red if the answer is wrong
         answerIsWrong();
+    
+        
     }
-    count = 0;
+    // count = 0;
     if(runningQuestion < lastQuestion){
         runningQuestion++;
         renderQuestion();
     }else{
         // end the quiz and show the score
-        //clearInterval(TIMER);
-        scoreRender();
+        // clearInterval(TIMER);
+        // count = 0
+    scoreRender();
+    // scoreRender();
     }
 }
 
-// answer is correct
+//funcction to check in an anwer is correct
 function answerIsCorrect(){
     document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
 }
 
-// answer is Wrong
+//function to check if an answer is wrong
 function answerIsWrong(){
     document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+    count -=3
 }
 
-// score render
+//function to display the score once the user has completed the quiz. 
 function scoreRender(){
     scoreDiv.style.display = "block";
-    
-    // calculate the amount of question percent answered by the user
+    //clearInterval(TIMER);
+    count = 0
+    //calculate the amount of question percent answered by the user
     const scorePerCent = Math.round(100 * score/questions.length);
     
-    // choose the image based on the scorePerCent
+    //choose the image based on the scorePerCent the user got on the quiz
     let img = (scorePerCent >= 80) ? "assets/5.jpg" :
               (scorePerCent >= 60) ? "assets/4.jpg" :
               (scorePerCent >= 40) ? "assets/3.jpg" :
