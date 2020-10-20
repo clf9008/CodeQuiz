fe454252b41002aa8af5c10a8f8ac78bddfc36f0
Follett <clf9008@gmail.com>
2020-10-19 18:56:55 -0600

    updated scoreDisplay.css

--- scoreDisplay.js
+++ scoreDisplay.js
@@ -2,5 +2,5 @@ const highScoreList = document.getElementById("highScore");
 const highScores = JSON.parse(localStorage.getItem('highScore')) || [];
 highScoreList.innerhtml = highScore
 .map( score => {
-    return '<li class="high-score">${score.name}-${score.score}</li>';
+    return '<li class="high-score">${score.name} -${score.score}</li>';
 }).join("");
